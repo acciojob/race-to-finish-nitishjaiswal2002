7f5b0219143925c82407a484e21baf003b1a66f8
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-11-01 15:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,14 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-function getRandomTime() {
-  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
-}
-
-function createRandomDelayPromise() {
-  const delay = getRandomTime();
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Promise resolved after ${delay} milliseconds`);
-    }, delay);
-  });
-}
-
-const promises = Array.from({ length: 5 }, createRandomDelayPromise);
-
 Promise.any(promises)
   .then((result) => {
-    const outputElement = document.getElementById('output');
-    outputElement.textContent = result;
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
   })
   .catch((error) => {
     console.error(error);
   });
-
 
 
 
