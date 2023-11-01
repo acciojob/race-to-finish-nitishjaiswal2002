d03bc81a31e2664832ef8be65767744d984e136a
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-11-01 16:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+unction createPromise() {
+  const randomDelay = Math.floor(Math.random() * 5) + 1; // Random number between 1 and 5
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${randomDelay} seconds`);
+    }, randomDelay * 1000);
+  });
+}
+
+// Create an array of 5 promises
+const promises = Array.from({ length: 5 }, createPromise);
+
 Promise.any(promises)
   .then((result) => {
     const outputDiv = document.getElementById('output');
